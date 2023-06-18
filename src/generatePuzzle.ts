156876dd5cd23b8b59a879c1edd76ddeaa6c97b6
HerbCaudill <herb@devresults.com>
2023-06-18 15:20:13 +0200

    generatePuzzle.ts: use composite seed

--- src/generatePuzzle.ts
+++ src/generatePuzzle.ts
@@ -6,9 +6,9 @@ import { range } from 'utils/range'
 const INCLUDE_RATIO = 0.5 // % of cells to include
 
 export const generatePuzzle = ({ size = 3, seed = undefined }: Props = {}): PuzzleState => {
-  const rand = makeRandom(seed)
+  const compositeSeed = seed ? `${seed}_${size}` : undefined
+  const rand = makeRandom(compositeSeed)
   const nums = range(0, size - 1)
-
   const rows: Cell[][] = nums.map(row =>
     nums.map(col => ({
       coordinates: { row, col },
@@ -17,7 +17,6 @@ export const generatePuzzle = ({ size = 3, seed = undefined }: Props = {}): Puzz
       state: 'EMPTY',
     }))
   )
-
   return expandState(rows)
 }
 
