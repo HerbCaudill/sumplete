595f6f87bda8996ea6b62920bd7250de0294665a
Herb Caudill <herb@devresults.com>
2025-05-04 18:23:57 +0200

    add sw.ts

---
+++ src/sw.ts
@@ -0,0 +1,21 @@
+import { clientsClaim, skipWaiting } from 'workbox-core'
+import {
+  cleanupOutdatedCaches,
+  createHandlerBoundToURL,
+  precacheAndRoute,
+} from 'workbox-precaching'
+import { NavigationRoute, registerRoute } from 'workbox-routing'
+
+declare let self: ServiceWorkerGlobalScope
+
+// self.__WB_MANIFEST is default injection point
+precacheAndRoute(self.__WB_MANIFEST)
+
+// clean old assets
+cleanupOutdatedCaches()
+
+// to allow work offline
+registerRoute(new NavigationRoute(createHandlerBoundToURL('index.html')))
+
+skipWaiting()
+clientsClaim()
