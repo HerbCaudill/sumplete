7fcd568d3d60c5991b1c010c6a5b5428f34f06d8
Herb Caudill <herb@devresults.com>
2025-05-04 14:50:27 +0200

    downgrade tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
-/** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
+import { Config as TailwindConfig } from 'tailwindcss'
+import colors from 'tailwindcss/colors'
 
 const emoji = 'Segoe UI Emoji'
 const mono = 'IBM Plex Mono'
@@ -7,16 +7,16 @@ const sans = 'IBM Plex Sans'
 const condensed = 'IBM Plex Sans Condensed'
 const serif = 'IBM Plex Serif'
 
-module.exports = {
+const tailwindConfig: TailwindConfig = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  safelist: [4, 5, 6, 7, 8, 9, 10].map(x => `grid-cols-${x}`), // MIN_SIZE+1...MAX_SIZE+1 (keep in sync with src\constants.ts)
+  safelist: [4, 5, 6, 7, 8, 9, 10].map((x) => `grid-cols-${x}`), // MIN_SIZE+1...MAX_SIZE+1 (keep in sync with src\constants.ts)
   theme: {
     extend: {
       fontFamily: {
         mono: [mono, emoji, 'monospace'],
         sans: [sans, emoji, 'sans-serif'],
         condensed: [condensed, emoji, 'sans-serif'],
-        serif: [serif, emoji, 'serif'],
+        serif: [serif, emoji, 'serif']
       },
       colors: {
         primary: colors.blue,
@@ -24,20 +24,22 @@ module.exports = {
         neutral: colors.gray,
         success: colors.green,
         warning: colors.orange,
-        danger: colors.red,
+        danger: colors.red
       },
       fontWeight: {
-        thin: 100,
-        extralight: 200,
-        light: 300,
-        normal: 400,
-        text: 450,
-        medium: 500,
-        semibold: 600,
-        bold: 700,
-        extrabold: 800,
-      },
-    },
+        thin: '100',
+        extralight: '200',
+        light: '300',
+        normal: '400',
+        text: '450',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        extrabold: '800'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
+
+export default tailwindConfig
--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,43 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import type { VitePWAOptions } from 'vite-plugin-pwa'
-import { VitePWA as vitePWA } from 'vite-plugin-pwa'
 import autoImport from 'unplugin-auto-import/vite'
 import iconsResolver from 'unplugin-icons/resolver'
 import icons from 'unplugin-icons/vite'
+import { defineConfig } from 'vite'
+import { VitePWA as vitePWA } from 'vite-plugin-pwa'
 import tsconfigPaths from 'vite-tsconfig-paths'
-
-const pwaOptions: Partial<VitePWAOptions> = {
-  mode: 'production',
-  includeAssets: ['favicon.ico'],
-  srcDir: 'src',
-  filename: 'sw.ts',
-  registerType: 'autoUpdate',
-  strategies: 'injectManifest',
-  injectManifest: {
-    globPatterns: ['**/*.{js,css,html,ico,png,json,svg,txt}'],
-  },
-  manifest: {
-    name: 'Sumplete',
-    short_name: 'Sumplete',
-    theme_color: '#e7e5e4',
-    background_color: '#e7e5e4',
-    display: 'standalone',
-    icons: [
-      { src: 'favicon.svg', sizes: '1024x1024', type: 'image/svg+xml', purpose: 'any' },
-      // { src: 'favicon-solid.png', sizes: '1024x1024', type: 'image/png', purpose: 'any' },
-      // { src: 'favicon.png', sizes: '1024x1024', type: 'image/png', purpose: 'maskable' },
-    ],
-  },
-}
 
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
-    vitePWA(pwaOptions),
+    vitePWA({
+      mode: 'production',
+      includeAssets: ['favicon.ico'],
+      srcDir: 'src',
+      filename: 'sw.ts',
+      registerType: 'autoUpdate',
+      strategies: 'injectManifest',
+      injectManifest: {
+        globPatterns: ['**/*.{js,css,html,ico,png,json,svg,txt}']
+      },
+      manifest: {
+        name: 'Sumplete',
+        short_name: 'Sumplete',
+        theme_color: '#e7e5e4',
+        background_color: '#e7e5e4',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'favicon.svg',
+            sizes: '1024x1024',
+            type: 'image/svg+xml',
+            purpose: 'any'
+          }
+          // { src: 'favicon-solid.png', sizes: '1024x1024', type: 'image/png', purpose: 'any' },
+          // { src: 'favicon.png', sizes: '1024x1024', type: 'image/png', purpose: 'maskable' },
+        ]
+      }
+    }),
     autoImport({
       dts: false,
       resolvers: [
@@ -43,12 +45,10 @@ export default defineConfig({
           prefix: false,
           extension: 'jsx',
           enabledCollections: ['tabler'],
-          alias: {
-            icon: 'tabler',
-          },
-        }),
-      ],
+          alias: { icon: 'tabler' }
+        })
+      ]
     }),
-    icons({ compiler: 'jsx', jsx: 'react' }),
-  ],
+    icons({ compiler: 'jsx', jsx: 'react' })
+  ]
 })
