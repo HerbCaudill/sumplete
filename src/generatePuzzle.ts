d6c95fb992803deefe1fe8c9c6ec9a29ed752c6f
Herb Caudill <herb@devresults.com>
2025-05-16 12:40:49 +0200

    don't check for unique solutions

--- src/generatePuzzle.ts
+++ src/generatePuzzle.ts
@@ -1,14 +1,14 @@
-import { makeRandom } from '@herbcaudill/random'
-import { expandState } from 'expandState'
-import { hasUniqueSolution } from 'solve'
-import { PuzzleState, PuzzleCell } from 'types'
-import { range } from 'utils/range'
+import { makeRandom } from "@herbcaudill/random"
+import { expandState } from "expandState"
+import { hasUniqueSolution } from "solve"
+import { PuzzleState, PuzzleCell } from "types"
+import { range } from "utils/range"
 
 const INCLUDE_RATIO = 0.5 // % of cells to include
 
 export const generatePuzzle = ({
   size = 3,
-  seed = Math.random().toString(36)
+  seed = Math.random().toString(36),
 }: Props = {}): PuzzleState => {
   const compositeSeed = `${seed}_${size}`
   const rand = makeRandom(compositeSeed)
@@ -19,19 +19,11 @@ export const generatePuzzle = ({
       col,
       value: rand.integer(1, 9),
       included: rand.probability(INCLUDE_RATIO),
-      state: '?'
-    }))
+      state: "?",
+    })),
   )
 
-  const expanded = expandState({ rows, startTime: Date.now() })
-
-  const { rowTargets, colTargets } = expanded
-
-  if (hasUniqueSolution({ rows, rowTargets, colTargets })) {
-    return expanded
-  } else {
-    return generatePuzzle({ size, seed: seed + '!' })
-  }
+  return expandState({ rows, startTime: Date.now() })
 }
 
 type Props = {
